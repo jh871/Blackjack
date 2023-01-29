7b4fdacda30b196e04b1faa070635dd604705b81
jh871 <jennyh871@gmail.com>
2023-01-29 13:37:34 +0000

    added HTML and js files

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+let playerScore = 0;
+let dealerScore = 0;
+
+// alert("player's score is " + playerScore);
+
+var game {
+    player: 0;
+    dealer: 0;
+    goal : 21;
+    bust: > 21;
+};
+
+
+playerStart = //random 4-21
+randomNum = //random 2-11
+dealerStart = randomNum;
+
+// playerScore = playerScore + i
+
+//if player and dealer are <21, largest score wins
